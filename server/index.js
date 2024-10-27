772db15869d13ef3fa7082ee496553db51d89c7c
Emre Ercelebi <30380456+emreercelebi@users.noreply.github.com>
2024-10-27 17:48:58 -0400

    connect to google sheets api

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const { google }  = require('googleapis')
+const authData = require('./credentials')
 
 app.use(cors())
 
@@ -10,6 +12,27 @@ app.listen(8080, () => {
       console.log('server listening on port 8080')
 })
 
-app.get('/', (req, res) => {
-  res.send('Hello from our server!')
+app.get('/', async (req, res) => {
+  const sheetsData = await getSheetsData();
+  res.send(sheetsData);
 })
+
+async function getSheetsData() {
+  const { private_key, client_email } = authData.credentials;
+  const sheetName = 'Sep 2024';
+
+  const auth = new google.auth.JWT({
+    email: client_email,
+    key: private_key,
+    scopes: ["https://www.googleapis.com/auth/spreadsheets"]
+  })
+  const sheet = google.sheets("v4")
+
+  const data = await sheet.spreadsheets.values.get({
+    spreadsheetId: authData.spreadsheetId,
+    range: `${sheetName}!A1:D100`,
+    auth,
+  });
+
+  return data;
+}
