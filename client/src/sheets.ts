1539e24399ed9d6d00d2c6dac9c49a2dbadb8ff4
Emre Ercelebi <30380456+emreercelebi@users.noreply.github.com>
2024-10-28 22:10:50 -0400

    get individual sheets

--- client/src/sheets.ts
+++ client/src/sheets.ts
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 
 export async function getSpendingData() {
-  const data = await axios.get('http://localhost:8080');
+  const data = await axios.get('http://localhost:8080/year/2024');
   console.log('data', data);
 }
--- server/index.js
+++ server/index.js
@@ -3,23 +3,31 @@ const app = express();
 const cors = require('cors');
 const { google }  = require('googleapis')
 const authData = require('./credentials')
-
 app.use(cors())
 
-console.log(app)
+// const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+const months = ['Sep','Oct'];
+
 
 app.listen(8080, () => {
       console.log('server listening on port 8080')
 })
 
-app.get('/', async (req, res) => {
-  const sheetsData = await getSheetsData();
-  res.send(sheetsData);
+app.get('/year/:year', async (req, res) => {
+  const year = req.params.year;
+  const data = {};
+
+  await Promise.all(months.map(month => {
+    const getMonthData = async (data, month, year) => {
+      data[month] = await getSheetsData(`${month} ${year}`);
+    }
+    return getMonthData(data, month, year);
+  }))
+  res.send(data);
 })
 
-async function getSheetsData() {
+async function getSheetsData(sheetName) {
   const { private_key, client_email } = authData.credentials;
-  const sheetName = 'Sep 2024';
 
   const auth = new google.auth.JWT({
     email: client_email,
